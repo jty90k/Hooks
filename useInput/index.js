4952dc4a6ac4f35eb520d5d94d8c7b0060b6e438
UniverseStark <jty90k@gmail.com>
2021-04-01 11:13:27 +0900

    useStat 1, 2

---
+++ useInput/index.js
@@ -0,0 +1,29 @@
+// useState 1
+export const useInput = (initalValue, validator) => {
+  const [value, setValue] = useState(initalValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+// useState 2
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
